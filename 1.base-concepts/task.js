81fef6c16bd4474ddf581d1f583bddbca716d537
Kkatusha.a <katusha.25092003@mail.ru>
2025-06-29 00:34:44 +0300

    Исправления 2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,15 +1,15 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  let d = b ** 2 - 4* a * c
+  let d = b ** 2 - 4* a * c;
 
 
-  if (d  === 0) {
-    arr = [b/(2*a)];
+  if (d === 0) {
+    arr = [(b/(2*a))];
   } else if (d > 0) {
-    let const1 = (-b + Math.sqrt(d) )/(2*a)
-    let const2 = (-b - Math.sqrt(d) )/(2*a)
-    arr = [const1, const2]
+    let const1 = (-b + Math.sqrt(d) )/(2*a);
+    let const2 = (-b - Math.sqrt(d) )/(2*a);
+    arr = [const1, const2];
   }
   return arr;
 }
