eb56f6630b979cb3cf48ad9ab921f89c417a097c
Kkatusha.a <katusha.25092003@mail.ru>
2025-06-30 00:39:25 +0300

    Задание 1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,5 +1,17 @@
 function getArrayParams(...arr) {
-
+let min = Infinity;
+let max = -Infinity;
+let sum = min+max;
+for (let i=0; i < arr.length; i++) {
+  const num = arr[i];
+if ( num > max ) {
+  let max = num;
+} else if ( num < min) {
+  let min = num;
+}
+}
+  sum += num;
+const avg = (sum/arr.length).toFixed(2);
   return { min: min, max: max, avg: avg };
 }
 
